76d594a2d9de5683a45a0ce76b314044ea1c187b
Kliment Mamykin <kmamyk@yahoo.com>
2010-04-24 15:15:20 -0400

    Added receive_email

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,5 +1,5 @@
 class TwilioController < ApplicationController
-  protect_from_forgery :except => :sms
+  protect_from_forgery :except => [:sms, :receive_email]
 
   def sms
     @message = "Thank you for sms"
@@ -10,5 +10,19 @@ class TwilioController < ApplicationController
     PatientMailer.deliver_send_mail()
     render :inline => true, :text => "Sent"
   end
-  
+
+  def receive_email
+    #@params = params
+    # create email based on params here
+    respond_to do |format|
+      if request.post? # and email saves correctly
+        # process email
+        flash[:notice] = "Email processed"
+        format.xml { render :xml => {:element => "Element"}, :status => :created }
+      else
+        flash[:error] = "Oops! Error processing email"
+        format.xml { render :xml => {:error => "Error"}, :status => :unprocessable_entity }
+      end
+    end
+  end
 end
