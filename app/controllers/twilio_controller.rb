efb4a03a58581fb6743b6ee0bd16d63b828d3d5a
Kliment Mamykin <kmamyk@yahoo.com>
2010-04-24 15:24:55 -0400

    just rendering plain text as response to receive_email

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -14,15 +14,16 @@ class TwilioController < ApplicationController
   def receive_email
     #@params = params
     # create email based on params here
-    respond_to do |format|
-      if request.post? # and email saves correctly
-        # process email
-        flash[:notice] = "Email processed"
-        format.xml { render :xml => {:element => "Element"}, :status => :created }
-      else
-        flash[:error] = "Oops! Error processing email"
-        format.xml { render :xml => {:error => "Error"}, :status => :unprocessable_entity }
-      end
-    end
+#    respond_to do |format|
+#      if request.post? # and email saves correctly
+#        # process email
+#        flash[:notice] = "Email processed"
+#        format.xml { render :xml => {:element => "Element"}, :status => :created }
+#      else
+#        flash[:error] = "Oops! Error processing email"
+#        format.xml { render :xml => {:error => "Error"}, :status => :unprocessable_entity }
+#      end
+#    end
+    render :text => "Processed"
   end
 end
