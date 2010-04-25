0b22f0a25a89243930bf47a4915fb1ba3eac92f8
Kliment Mamykin <kmamyk@yahoo.com>
2010-04-25 12:10:16 -0400

    Added formtastic 0.9.8

--- config/environment.rb
+++ config/environment.rb
@@ -37,6 +37,7 @@ Rails::Initializer.run do |config|
   config.gem "mms2r", :version => "3.0.1"
   config.gem "paperclip", :version => "2.3.1.1"
   config.gem "authlogic", :version => "2.1.3"
+  config.gem "formtastic", :version => "0.9.8"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
