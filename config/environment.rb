10077cace7130d0a4f1f48ec395ae76641231983
Kliment Mamykin <kmamyk@yahoo.com>
2010-04-25 10:54:41 -0400

    Added MMS2R gem

--- config/environment.rb
+++ config/environment.rb
@@ -34,6 +34,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "twiliolib", :lib => "twiliolib", :version => "2.0.4"
+  config.gem "mms2r", :version => "3.0.1"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
